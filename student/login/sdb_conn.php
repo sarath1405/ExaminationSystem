23dbde61da8fcaf80e1dc821827511e71e28fb67
sarath1405 <sarathchandra1426@gmail.com>
2022-01-30 22:40:37 +0530

    commit5

--- student/login/sdb_conn.php
+++ student/login/sdb_conn.php
@@ -1,8 +1,8 @@
 <?php 
 
-$s_name = "localhost";
-$user = "root";
-$password = "";
+$s_name = "mysql-67603-0.cloudclusters.net";
+$user = "admin";
+$password = "Zur00QVU";
 $db_name = "student_db";
 
 $conn = mysqli_connect($s_name, $user, $password, $db_name);
