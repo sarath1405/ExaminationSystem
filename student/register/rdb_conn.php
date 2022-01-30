c69e5c3d0205afe1f4683f2e5cfca958039d62b3
sarath1405 <sarathchandra1426@gmail.com>
2022-01-30 22:41:27 +0530

    commit6

--- student/register/rdb_conn.php
+++ student/register/rdb_conn.php
@@ -1,11 +1,11 @@
-<?php
+<?php 
 
-$s_name_ = "localhost";
-$username_ = "root";
-$password_ = "";
-$db_name_ = "student_db";
+$s_name = "mysql-67603-0.cloudclusters.net";
+$user = "admin";
+$password = "Zur00QVU";
+$db_name = "student_db";
 
-$conn = mysqli_connect($s_name_, $username_, $password_, $db_name_);
+$conn = mysqli_connect($s_name, $user, $password, $db_name);
 
 if(!$conn) {
     echo "DataBase connection failed, Try again!";
